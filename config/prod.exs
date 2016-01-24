9bb5aa6856b2fcdb60c32139235f32a40258ade0
Ryan Troxler <rtroxler@rednovalabs.com>
2016-01-23 22:22:01 -0600

    Use production config for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -13,11 +13,19 @@ use Mix.Config
 # which you typically run after static files are built.
 config :rnl_hackathon, RnlHackathon.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [host: "ancient-harbor-12366.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+# Configure your database
+config :rnl_hackathon, RnlHackathon.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20
 
 # ## SSL Support
 #
@@ -60,6 +68,3 @@ config :logger, level: :info
 #
 #     config :rnl_hackathon, RnlHackathon.Endpoint, root: "."
 
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
