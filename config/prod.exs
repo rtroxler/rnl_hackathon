f5154136646ba6844e7303cf5b92e9ba32fd1bdf
Ryan Troxler <rtroxler@rednovalabs.com>
2016-01-25 13:33:30 -0600

    Update prod

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :rnl_hackathon, RnlHackathon.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "ancient-harbor-12366.herokuapp.com", port: 443],
+  url: [host: "rnl-hackathon.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
