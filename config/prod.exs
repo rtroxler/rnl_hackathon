bae6074f6301717073e3e9c92cfe81d0b7ffccb9
Ryan Troxler <rtroxler@rednovalabs.com>
2016-01-25 13:40:21 -0600

    no ssl

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :rnl_hackathon, RnlHackathon.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "rnl-hackathon.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "hackathon.rnl.io", port: 80],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
